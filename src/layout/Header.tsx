b95b1a3f26edbf1a8883477fc6c29e70542e8d97
Kaizouku14 <mandaalv72@gmail.com>
2024-07-10 21:27:58 +0800

    feat : added scrollarea and srollbar in mobile view

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,14 +1,14 @@
-import { CircleUser, Search, X } from 'lucide-react';
+import { Search, X } from 'lucide-react'; 
 import { useState } from 'react';
+import { Avatar, AvatarFallback, AvatarImage } from '../components/ui/avatar';
 
 const Header = () => {
   const [isSearchFocused, setIsSearchFocused] = useState(false);
   const [history, setHistory] = useState([]);
 
   return (
-    <header className="w-full h-20 flex items-center justify-between px-4 sm:px-6 border-b border-gray-700">
-      <img
-        src="logo.png"
+    <header className="w-full h-20 flex items-center justify-between px-4 sm:px-6">
+      <img src="logo.png"
         alt="reel-hub-logo"
         className="h-12 sm:h-16 max-md:h-12"
       />
@@ -30,19 +30,18 @@ const Header = () => {
         />
 
         <div className={`${history.length > 0 ? 'flex' : 'hidden'} absolute md:w-[290px] h-52 rounded bg-slate-800 flex-col top-12 md:top-[2.75rem] py-2 px-2 shadow-lg gap-y-1 max-md:hidden`}>
-           <span className='text-[0.65rem] text-slate-200 text-end '>Recent searched</span>
+           <span className='text-[0.65rem] text-slate-200 text-end font-'>Recent searched</span>
           <div className="flex justify-between items-center text-sm py-2 text-slate-400 border-b border-slate-400">
             Jurassic Park
             <X className="hover:text-slate-500 cursor-pointer" size={20} />
           </div>
           {/* Add more search results here */}
         </div>
-
       </div>
-      <CircleUser
-        className="text-slate-400"
-        size={24}
-      />
+      <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+        <AvatarFallback>CN</AvatarFallback>
+     </Avatar>
     </header>
   );
 };
--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -1,8 +1,56 @@
+import {  ScrollArea , ScrollBar   } from "../components/ui/scroll-area";
 
 const Main = () => {
+  const movies = [
+    { id: 1, title: 'Hit Man', imgSrc: 'movi-poster.jpg' },
+    { id: 2, title: 'Hit Man', imgSrc: 'movi-poster.jpg' },
+    { id: 3, title: 'Hit Man', imgSrc: 'movi-poster.jpg' },
+    { id: 4, title: 'Hit Man', imgSrc: 'movi-poster.jpg' },
+    { id: 5, title: 'Hit Man', imgSrc: 'movi-poster.jpg' },
+    { id: 6, title: 'Hit Man', imgSrc: 'movi-poster.jpg' },
+    { id: 7, title: 'Hit Man', imgSrc: 'movi-poster.jpg' },
+    { id: 8, title: 'Hit Man', imgSrc: 'movi-poster.jpg' },
+    { id: 6, title: 'Hit Man', imgSrc: 'movi-poster.jpg' },
+    { id: 7, title: 'Hit Man', imgSrc: 'movi-poster.jpg' },
+    { id: 8, title: 'Hit Man', imgSrc: 'movi-poster.jpg' },
+    { id: 6, title: 'Hit Man', imgSrc: 'movi-poster.jpg' },
+    { id: 7, title: 'Hit Man', imgSrc: 'movi-poster.jpg' },
+    { id: 8, title: 'Hit Man', imgSrc: 'movi-poster.jpg' },
+    { id: 6, title: 'Hit Man', imgSrc: 'movi-poster.jpg' },
+    { id: 7, title: 'Hit Man', imgSrc: 'movi-poster.jpg' },
+    { id: 8, title: 'Hit Man', imgSrc: 'movi-poster.jpg' },
+    { id: 6, title: 'Hit Man', imgSrc: 'movi-poster.jpg' },
+    { id: 7, title: 'Hit Man', imgSrc: 'movi-poster.jpg' },
+    { id: 8, title: 'Hit Man', imgSrc: 'movi-poster.jpg' },
+  ];
+
+
   return (
-    <main className="border-2 h-[585px] ">
-        Main
+    <main className="h-[585px] flex flex-col p-3 gap-y-2">
+
+        <section className="p-4 h-76 flex flex-col gap-y-3 text-white ">
+           <span className="text-lg max-md:text-base font-bold">Continue Watching</span>
+
+            <ScrollArea className="flex w-full space-x-2 whitespace-nowrap">
+              <div className="flex w-max space-x-4 p-4">
+                  {movies.map((movie) => (
+                    <div key={movie.id} className="flex flex-col items-center rounded-xl w-36 mx-1">
+                      <img
+                        src={movie.imgSrc}
+                        alt="Movie Poster"
+                        className="cursor-pointer rounded-xl hover:border-2 h-48 w-36 object-cover"
+                      />
+                      <span className="text-base text-white mt-2 ">{movie.title}</span>
+                    </div>
+                  ))}
+              </div>
+               <ScrollBar className="hidden max-md:block"
+                 orientation="horizontal"  />
+            </ScrollArea>
+
+        </section>
+
+
     </main>
   )
 }
